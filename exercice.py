fa86b25412a9fa649d4078da4e3ff094713dce3d
MajeedAB <majeed.abdul-baki@polymtl.ca>
2021-09-28 11:15:29 -0400

    exo fini

--- exercice.py
+++ exercice.py
@@ -1,52 +1,101 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import statistics
 
 def order(values: list = None) -> list:
     if values is None:
+        values = []
         # TODO: demander les valeurs ici
-        pass
-
-    return []
+        for i in range(10):
+            values.append(float(input("Valeur "+str(i)+"\n")))
+    print(values)
+    values.sort()
+    return values
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: demander les mots ici
-        pass
-
-    return False
+        words = []
+        words.append(input("premier mot"+"\n"))
+        words.append(input("deuxieme mot"+"\n"))
+    for lettre in words[0]:
+        if (lettre in words[1]):
+            words[1] = words[1].replace(lettre,'',1)
+        else: #la lettre n'est pas dans l'autre mot, donc pas des anagrames
+            print("Pas des anagrames!")
+            return False
+    #fini de parcourir mot 1:
+    if(words[1]==""): #il ne reste plus de lettres dans le 2e mot, donc reelement des anagrames
+        print("OUI, anagrames!")
+        return True
+    else: #il y a plus de lettres dans le 2e mot
+        print("Pas des anagrames!")
+        return False
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    ensemble = {item for item in items}
+    return not len(ensemble)==len(items) #si len sont egaux alors uniques, donc ne contient PAS de doublons
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    best = {}
+    best_av = 0
+    best_av_person = ""
+    for name in student_grades:
+        av = 0
+        for grade in student_grades[name]:
+            av += grade
+        av/=len(student_grades[name])
+        if(av>=best_av):
+            best_av = av
+            best_av_person = name
+    best = {best_av_person:best_av}
+    return best
+
+    # return student_grades[sorted( student_grades,key = lambda x: (x.statistics.mean() for x in student_grades) )[0]]
+
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    lettres_frequentes = {}
+    for lettre in sentence:
+        # if(sentence.count(lettre)>5):
+        lettres_frequentes[lettre]=sentence.count(lettre)
+    
+    lettres_ordonnees = dict(sorted(lettres_frequentes.items(),key= lambda x:-x[1]))
+    for lettre in lettres_ordonnees:
+        print(lettre,"apparait ",lettres_ordonnees[lettre],"fois")
+    print(lettres_ordonnees)
+    return lettres_ordonnees
 
-    return {}
-
-
+recipes = {}
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    nom = input("nom de la recette:\n")
+    nb = int(input("combien d'ingredients?\n"))
+    recipes[nom]=[]
+    for i in range(nb):
+        recipes[nom].append(input("Ingredient:\n"))
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    nom = input("quel recette cherche-tu?\n")
+    if(not nom in recipes):
+        print("on n'a pas cette recette")
+    else:
+        print("La recette de",nom,"a pour ingredients:")
+        for ing in recipes[nom]:
+            print(ing)
 
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
-    order()
+    print(order())
 
     print(f"On vérifie les anagrammes...")
     anagrams()
